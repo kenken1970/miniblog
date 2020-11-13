1d2487dfff34d3b83e25e75f64114edb3b85da98
EC2 Default User <ec2-user@ip-172-31-1-103.ap-northeast-1.compute.internal>
2020-11-13 14:06:07 +0000

    初めてのアプリ

---
+++ app/controllers/tweets_controller.rb
@@ -0,0 +1,46 @@
+class TweetsController < ApplicationController
+  before_action :authenticate_user!, except: [:index]  
+
+  def new
+    @tweet = Tweet.new  
+  end
+
+  def create
+    @tweet = Tweet.new(tweet_params)  
+    @tweet.user_id = current_user.id 
+    @tweet.save
+    redirect_to tweets_path
+  end
+
+  def index
+    @tweets = Tweet.all
+  end
+
+  def show
+    @tweet = Tweet.find(params[:id])
+    @user = @tweet.user
+  end
+  
+  def destroy
+    tweet = Tweet.find(params[:id])
+    if tweet.user_id == current_user.id
+     tweet.destroy 
+    end
+  end
+  
+  def edit
+    @tweet = Tweet.find(params[:id])
+  end
+
+  def update
+    tweet = Tweet.find(params[:id])
+    if tweet.user_id == current_user.id
+      tweet.update(tweet_params)
+    end
+  end
+
+  private
+    def tweet_params
+      params.require(:tweet).permit(:body) 
+    end
+end
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,13 @@
+class UsersController < ApplicationController
+  before_action :authenticate_user!
+  def index
+    @users = User.all
+  end
+
+  def show
+    @user = User.find(params[:id])
+# ===============追加==============
+    @tweets = @user.tweets
+# ================================
+  end
+end
